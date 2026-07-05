a6fd5dea2c55e5eeebf709b25879cfcf92b9bf04
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-07-05 05:49:55 +0530

    Time: 1035 ms (64.51%), Space: 248.2 MB (32.92%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -3,9 +3,7 @@ public:
     vector<unordered_map<int, int>> d;
 
     long long f(vector<int>&a, int i, int j,int diff) {
-        // if (dp[i].count(diff))
-        //     return dp[i][diff];
-
+    
         long long ans = 0;
         if(d[j].count(diff)==0){
             ans=2;
@@ -14,18 +12,6 @@ public:
         else{
             ans=1+d[j][diff];
         }
-
-        // if(i!=0){
-
-        // }
-
-        // for (int j = i-1; j >- 0; j--) {
-
-        //     if (1LL * a[i] - a[j] == diff) {
-
-        //         ans = max(ans,1+ f(j, diff));
-        //     }
-        // }
 
         return d[i][diff] = ans;
     }
@@ -42,13 +28,10 @@ public:
 
         for (int i = 0; i < n; i++) {
 
-            unordered_set<long long> s;
-
-            for (int j = i-1; j>=0; j--) {
+            for (int j = 0; j<i; j++) {
 
                 int diff = a[i] - a[j];
 
-                if (s.insert(diff).second)
                     ans = max(ans, f(a,i, j, diff));
             }
         }
