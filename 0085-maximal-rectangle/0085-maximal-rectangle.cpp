340df02b015d0406918fa399c584efbb11fdc29e
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-07-08 16:05:43 +0530

    Time: 10 ms (44.15%), Space: 22.7 MB (22.53%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,99 @@
+class Solution {
+public:
+    int x = 0;
+
+    int f(vector<vector<int>>& a, int i) {
+
+        int n = a[i].size();
+
+        vector<int> ans(n);
+        stack<int> st;
+
+        // Next Smaller
+        for (int j = n - 1; j >= 0; j--) {
+
+            while (!st.empty() && a[i][st.top()] >= a[i][j])
+                st.pop();
+
+            if (st.empty())
+                ans[j] = n;
+            else
+                ans[j] = st.top();
+
+            st.push(j);
+        }
+
+        while (!st.empty())
+            st.pop();
+
+        vector<int> an(n);
+
+        // Previous Smaller
+        for (int j = 0; j < n; j++) {
+
+            while (!st.empty() && a[i][st.top()] >= a[i][j])
+                st.pop();
+
+            if (st.empty())
+                an[j] = -1;
+            else
+                an[j] = st.top();
+
+            st.push(j);
+        }
+
+        int m = 0;
+
+        for (int j = 0; j < n; j++) {
+
+            int width = ans[j] - an[j] - 1;
+            int area = width * a[i][j];
+
+            m = max(m, area);
+        }
+
+        return m;
+    }
+
+    int maximalRectangle(vector<vector<char>>& b) {
+
+        if (b.empty())
+            return 0;
+
+        int r = b.size();
+        int c = b[0].size();
+
+        vector<vector<int>> a(r, vector<int>(c));
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                a[i][j] = b[i][j] - '0';
+            }
+        }
+
+        x = f(a, 0);
+
+        int i = 1;
+
+        while (i < r) {
+
+            int j = 0;
+
+            while (j < c) {
+
+                if (a[i][j] != 0)
+                    a[i][j] += a[i - 1][j];
+                else
+                    a[i][j] = 0;
+
+                j++;
+            }
+
+            x = max(x, f(a, i));
+
+            i++;
+        }
+
+        return x;
+    }
+};
