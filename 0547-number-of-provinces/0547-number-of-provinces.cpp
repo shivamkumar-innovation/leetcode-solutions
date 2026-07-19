97087a6837b8abc376c1256e66b966bb399776ee
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-07-20 01:03:03 +0530

    Time: 0 ms (100%), Space: 19.5 MB (61.37%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -7,15 +7,16 @@ public:
         int t=0;
         while(i<a.size()){
             t++;
-            // a[i]=1;
+            a[i]=1;
             q.push(i);
             while(!q.empty()){
                 int b=q.front();
                 q.pop();
-                a[b]=1;
+                // a[b]=1;
                 int j=0;
                 while(j<v.size()){
                     if(v[b][j]==1 && !a[j]){
+                        a[j]=1;
                         q.push(j);
                     }
                     j++;
