6407188b62f287053f7d422a67b47662ac08a0d1
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-07-20 01:01:28 +0530

    Time: 7 ms (11.81%), Space: 19.9 MB (46.14%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& v) {
+        queue<int>q;
+        int i=0;
+        vector<int>a(v.size());
+        int t=0;
+        while(i<a.size()){
+            t++;
+            // a[i]=1;
+            q.push(i);
+            while(!q.empty()){
+                int b=q.front();
+                q.pop();
+                a[b]=1;
+                int j=0;
+                while(j<v.size()){
+                    if(v[b][j]==1 && !a[j]){
+                        q.push(j);
+                    }
+                    j++;
+                }
+            }
+            while(i<v.size() && a[i]!=0){
+                i++;
+            }
+        }
+        return t;
+    }
+};
+
